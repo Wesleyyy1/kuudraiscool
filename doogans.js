abf4f365c974cd01f4ce2e4537b4813a5f243419
bolding <mr.dick.151@gmail.com>
2024-08-18 22:25:28 +0200

    Small doogans change

--- doogans.js
+++ doogans.js
@@ -54,35 +54,28 @@ function processPlayerData(data) {
     const { name, uuid, members } = data;
     const memberData = members[uuid];
 
+    const quiverData = memberData.inventory?.bag_contents?.quiver?.data;
+    const soulflow = memberData.item_data?.soulflow || 0;
     let arrows = 0;
-    const data = memberData.inventory?.bag_contents?.quiver?.data;
+    let quiver = null;
 
     if (data) {
-        const quiver = decompress(data);
-
+        quiver = decompress(quiverData);
         if (quiver) {
             for (let i = 0; i < quiver.func_74745_c(); i++) {
-                let item = quiver.func_150305_b(i);
-                if (!item) continue;
-
-                let count = new NBTTagCompound(item).getByte("Count") || 1;
-                arrows += count;
+                const item = quiver.func_150305_b(i);
+                if (item) {
+                    arrows += new NBTTagCompound(item).getByte("Count") || 1;
+                }
             }
         }
-    } else {
+    }
+
+    if (!soulflow || !quiverData || !quiver) {
         msgData.messages.push(`&a${name} &f-> &7API OFF`);
         msgData.checked++;
         return;
     }
-
-    const soulflow = memberData.item_data?.soulflow || 0;
-
-    if (!soulflow) {
-        msgData.messages.push(`&a${name} &f-> &7API OFF`);
-        msgData.checked++;
-        return;
-    }
-
 
     let enoughArrows = arrows >= 433 || !arrows ? true : `GET ARROWS (&c${(arrows / 2880 * 100).toFixed(2)}%&7)`;
     let enoughSoulflow = soulflow >= 501 ? true : `GET SOULFLOW (&c${fixNumber(soulflow)}&7)`;
@@ -102,15 +95,15 @@ function checkCompleted() {
         msg.addTextComponent("&2&m-----&f[- &2doogans &f-]&2&m-----\n");
 
         if (msgData.messages.length == 0) {
-            msg.addTextComponent(new TextComponent("&aAll party members have sufficient soulflow and arrows!"));
+            msg.addTextComponent("&aAll party members have sufficient soulflow and arrows!");
         } else {
-            msg.addTextComponent(new TextComponent("&cThe following members have issues:\n\n"));
+            msg.addTextComponent("&cThe following members have issues:\n\n");
             msgData.messages.forEach(message => {
                 msg.addTextComponent(new TextComponent(`${message}\n`).setClick("run_command", `/ct copy ${ChatLib.removeFormatting(message)}`));
             });
         }
 
-        msg.addTextComponent(new TextComponent("&2&m-----------------------"));
+        msg.addTextComponent("&2&m-----------------------");
 
         ChatLib.chat(msg);
     }
